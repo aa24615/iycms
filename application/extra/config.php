1ecde1cd3160cdfcaf2205f2df48483694fad3a2
あ读鈊茚ケ <839024615@qq.com>
2018-04-22 23:37:57 +0800

    123

--- application/extra/config.php
+++ application/extra/config.php
@@ -1,10 +1,10 @@
 <?php
 	return [
-		'name' => 'iycms博客管理系统',
+		'name' => 'IYCMS博客管理系统',
 		'title' => '程序员的专用博客',
-		'keywords' => '程序员博客,vue博客,tp5博客',
+		'keywords' => 'IYCMS博客,程序员博客,vue博客,tp5博客',
 		'description' => '专为程序员订制的博客系统',
-		'powerby' => 'Copyright &copy; 2017-2018 IYCMS. 博客管理系统 版权所有',
+		'powerby' => 'Copyright © 2017-2018 IYCMS. 博客管理系统 版权所有',
 		'beian' => '赣ICP备16032092号',
 		'state_desc' => '服务器正在打瞌睡!',
 	];
